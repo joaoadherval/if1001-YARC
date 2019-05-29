3ff154c22fe9e9ca5684ec6354049f6c99736eb0
root <root@localhost.localdomain>
2019-05-29 14:15:23 -0300

    tentando o uso de extensões em android

--- YARC/app/src/main/java/cin/ufpe/br/yarc/features/news/NewsFragment.kt
+++ YARC/app/src/main/java/cin/ufpe/br/yarc/features/news/NewsFragment.kt
@@ -9,17 +9,28 @@ import android.view.View
 import android.view.ViewGroup
 import cin.ufpe.br.yarc.R
 import cin.ufpe.br.yarc.commons.inflate
+import kotlinx.android.synthetic.main.news_fragment.*
 
 class NewsFragment : Fragment() {
-    private var newsList : RecyclerView? = null
+    //testar o uso de extensões em android
+    //private var newsList : RecyclerView? = null
+    
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view = container?.inflate(R.layout.news_fragment)
+//        val view = container?.inflate(R.layout.news_fragment)
 
-        newsList = view?.findViewById(R.id.news_list) as RecyclerView?
-        newsList?.setHasFixedSize(true)
-        newsList?.layoutManager = LinearLayoutManager(context)
+//        newsList = view?.findViewById(R.id.news_list) as RecyclerView?
+//        newsList?.setHasFixedSize(true)
+//        newsList?.layoutManager = LinearLayoutManager(context)
 
-        return view
+//        return view
+        return container?.inflate(R.layout.news_fragment)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        news_list.setHasFixedSize(true)
+        news_list.layoutManager = LinearLayoutManager(context)
     }
 }
